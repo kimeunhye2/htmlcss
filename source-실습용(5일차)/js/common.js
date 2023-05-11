4b1bf89b48df4e61deaf30e2c67d0d1ff3a69ba5
DESKTOP-8UPBPUJ\학생 <phantom9512@naver.com>
2023-05-11 17:55:25 +0900

    no message

---
+++ source-실습용(5일차)/js/common.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(){
+    this.setTimeout(scrollTo, 0, 0, 0, 1);
+}, false);
+
+window.addEventListener("orientationchange", function(){
+    this.setTimeout(scrollTo, 0, 0 ,0, 1);
+}, false);
